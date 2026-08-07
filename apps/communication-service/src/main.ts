9d3107b6fde2f7731217ffb7a7dcba7a0527ea28
ErtanOzdemir <ertan_kubi@hotmail.com>
2026-08-07 13:46:32 +0300

    chore(communication-service): update http and debugger ports

--- apps/communication-service/src/main.ts
+++ apps/communication-service/src/main.ts
@@ -9,12 +9,12 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'communication-service';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3002;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Communication Service is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
